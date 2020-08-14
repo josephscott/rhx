66162ec672a932aa527de83ec4ac0e2109115e7c
Joseph Scott <joseph@josephscott.org>
2020-08-13 20:56:51 -0600

    Round out the get method

--- src/rhx.js
+++ src/rhx.js
@@ -1,7 +1,30 @@
-'use strict';
+"use strict";
 
 var rhx = {
+	// Issue GET request via XHR
+	//
+	// url: Target URL for the request
+	// options.on_load: Function called for on_load event
+	// options.on_error: Function called for on_error event
 	get( url, options ) {
+		// Defaults
+		var opt = {
+			on_load: function( e ) { console.log( e ); },
+			on_error: function( e ) { console.log( e ); }
+		};
 
+		// Over ride defaults with provided options
+		for ( var i in options ) {
+			opt[i] = options[i];
+		}
+
+		var xhr = new XMLHttpRequest();
+		xhr.addEventListener( "load", opt.on_load );
+		xhr.addEventListener( "error", opt.on_error );
+
+		xhr.open( "GET", url );
+		xhr.setRequestHeader( "X-RHX", 1 );
+
+		xhr.send();
 	}
 };
